c830dcc99b0527980de721ad5d01df56eb64914e
Isabella Marin <marin.isabella.10@gmail.com>
2025-06-24 11:31:18 -0700

    feat: initialized home page without nav bar or working buttons

---
+++ client/src/components/Home/Home.jsx
@@ -0,0 +1,23 @@
+import React from 'react';
+import { useNavigate } from 'react-router-dom';
+import './Home.css';
+
+const Home = () => {
+    const navigate = useNavigate();
+
+    return (
+        <>
+            <div className="home-container">
+                {/* NavBar */}
+                <div className="welcome-banner">
+                    <h1>Welcome to RoomMate!</h1>
+                    <h2>Find your next roommate today!</h2>
+                    <button className="hyperlink" onClick={() => navigate("/create-account")}>Click here to sign up and get started!</button>
+                </div>
+                <button className="hyperlink" onClick={() => navigate("/login")}>Login if you already have an account</button>
+            </div>
+        </>
+    )
+}
+
+export default Home;
