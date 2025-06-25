81c6aa0601b9f6beb6444fd4156ce1b6e6a2bc87
Isabella Marin <marin.isabella.10@gmail.com>
2025-06-24 17:58:06 -0700

    feat: implemented get users route, and create new user route

--- server/server.js
+++ server/server.js
@@ -1,6 +1,13 @@
 const express = require('express');
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 const app = express();
+const cors = require('cors');
+
+app.use(cors());
+app.use(express.json());
+
+const userRoutes = require('./api/user.js');
+app.use('/api/users', userRoutes)
 
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`);
