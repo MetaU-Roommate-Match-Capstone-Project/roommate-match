0a8fb92af3ca277a0435172b0acf520fa4104a56
Isabella Marin <marin.isabella.10@gmail.com>
2025-07-01 17:40:00 -0700

    feat: implemented modal that fetches other user profiles

---
+++ client/src/components/ProfileModal/ProfileModal.jsx
@@ -0,0 +1,112 @@
+import React from "react";
+import {
+  cleanlinessMap,
+  petsMap,
+  roomTypesMap,
+  sleepScheduleMap,
+  noiseToleranceMap,
+  socialnessMap,
+} from "../../utils/enums.jsx";
+
+const ProfileModal = ({ userProfile, onClose }) => {
+  if (!userProfile) {
+    return null;
+  }
+
+  let userCleanliness = cleanlinessMap[userProfile.cleanliness];
+  let userPets = petsMap[userProfile.pets];
+  let userRoomType = roomTypesMap[userProfile.room_type];
+  let numRoommates = userProfile.num_roommates;
+  let leaseDuration = userProfile.lease_duration;
+  let moveInDate = new Date(userProfile.move_in_date).toLocaleDateString();
+  let userSleepSchedule = sleepScheduleMap[userProfile.sleep_schedule];
+  let userNoiseTolerance = noiseToleranceMap[userProfile.noise_tolerance];
+  let userSocialness = socialnessMap[userProfile.socialness];
+  let favoriteMusic = userProfile.favorite_music;
+  let status = userProfile.user.intern_or_new_grad;
+  let budget = userProfile.user.budget_max;
+
+  return (
+    <>
+      <div className="modal-overlay" onClick={onClose}>
+        <div className="modal-content" onClick={(e) => e.stopPropagation()}>
+          <span className="x-btn" onClick={onClose}>
+            &times;
+          </span>
+          <div className="modal">
+            <div className="profile-container">
+              <div className="profile-card">
+                <h2>{userProfile.user?.name}</h2>
+                <div className="profile-details">
+                  <p>
+                    <strong>Location: </strong> {userProfile.city},{" "}
+                    {userProfile.state}
+                  </p>
+                  <p>
+                    <strong>Company: </strong> {userProfile.user?.company}
+                  </p>
+                  <p>
+                    <strong>University: </strong> {userProfile.user?.university}
+                  </p>
+                  <p>
+                    <strong>Status: </strong> {status}
+                  </p>
+                  <p>
+                    <strong>Budget: </strong> ${budget}.00
+                  </p>
+                  <p>
+                    <strong>Cleanliness: </strong> {userCleanliness}
+                  </p>
+                  <p>
+                    <strong>Smokes: </strong>
+                    {userProfile.smoke ? "Yes" : "No"}
+                  </p>
+                  <p>
+                    <strong>Pets: </strong> {userPets}
+                  </p>
+                  <p>
+                    <strong>Room Type: </strong> {userRoomType}
+                  </p>
+                  <p>
+                    <strong>Number of Roommates I am looking for: </strong>{" "}
+                    {numRoommates}
+                  </p>
+                  <p>
+                    <strong>Move In Date: </strong> {moveInDate}
+                  </p>
+                  <p>
+                    <strong>Lease Duration: </strong>
+                    {leaseDuration}
+                  </p>
+                  <p>
+                    <strong>Sleep Schedule: </strong> {userSleepSchedule}
+                  </p>
+                  <p>
+                    <strong>Noise Tolerance: </strong> {userNoiseTolerance}
+                  </p>
+                  <p>
+                    <strong>Socialness: </strong> {userSocialness}
+                  </p>
+                  <p>
+                    <strong>Hobbies: </strong>
+                    {userProfile.hobbies}
+                  </p>
+                  <p>
+                    <strong>Favorite Music: </strong>
+                    {favoriteMusic}
+                  </p>
+                  <p>
+                    <strong>Bio: </strong>
+                    {userProfile.bio}
+                  </p>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default ProfileModal;
--- client/src/pages/Dashboard/Dashboard.jsx
+++ client/src/pages/Dashboard/Dashboard.jsx
@@ -2,11 +2,14 @@ import React from "react";
 import { useState, useEffect } from "react";
 import { useUser } from "../../contexts/UserContext";
 import WithAuth from "../../components/WithAuth/WithAuth";
+import ProfileModal from "../../components/ProfileModal/ProfileModal";
 
 const Dashboard = () => {
   const { user } = useUser();
   const [posts, setPosts] = useState([]);
   const [error, setError] = useState(null);
+  const [userProfile, setUserProfile] = useState("");
+  const [showProfileModal, setShowProfileModal] = useState(false);
 
   const fetchPosts = async () => {
     try {
@@ -32,12 +35,38 @@ const Dashboard = () => {
     }
   };
 
+  const fetchUserProfile = async (id) => {
+    try {
+      const response = await fetch(`api/roommate-profile/${id}`, {
+        method: "GET",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        credentials: "include",
+      });
+
+      if (!response.ok) {
+        const errorInfo = await response.json();
+        throw new Error(errorInfo.error || "Failed to fetch user profile");
+      }
+
+      const profileFetched = await response.json();
+      setUserProfile(profileFetched);
+      setShowProfileModal(true);
+      setError(null);
+    } catch (error) {
+      console.error("Error fetching user profile: ", error);
+      setError(error.message);
+    }
+  };
+
   useEffect(
     () => {
       if (!user) {
         return;
       }
       fetchPosts();
+      fetchUserProfile();
     },
     [user],
     posts,
@@ -52,7 +81,12 @@ const Dashboard = () => {
           posts.map((post) => (
             <div key={post.id} className="post-card">
               <div className="post-header">
-                <p className="post-username">{post.user.name}</p>
+                <button
+                  className="post-username"
+                  onClick={() => fetchUserProfile(post.user.id)}
+                >
+                  {post.user.name}
+                </button>
               </div>
               <p className="post-location">
                 📍{post.city}, {post.state}
@@ -62,6 +96,13 @@ const Dashboard = () => {
           ))
         )}
       </div>
+
+      {showProfileModal && (
+        <ProfileModal
+          userProfile={userProfile}
+          onClose={() => setShowProfileModal(false)}
+        />
+      )}
     </>
   );
 };
