444789c726c49b239c67c32efe417065b02a895a
Isabella Marin <marin.isabella.10@gmail.com>
2025-07-10 15:11:03 -0700

    feat: temporary ranking slider for preferences on frontend

---
+++ client/src/components/RankingSlider/RankingSlider.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import { Slider } from "@material-tailwind/react";
+
+const RankingSlider = () => {
+    return (
+        <div>
+            <label className="form-label">Rank how important this attribute is to you:</label>
+            <Slider color="#3066BE" defaultValue={50} />
+        </div>
+    )
+
+}
+
+export default RankingSlider;
