0d2776dafbd99bc4ef30ff5624d79deecae1979a
Isabella Marin <marin.isabella.10@gmail.com>
2025-07-10 16:06:26 -0700

    feat: changed origin to string

--- server/server.js
+++ server/server.js
@@ -10,7 +10,7 @@ app.use(express.json());
 
 const corsConfig = cors({
     // origin: [/*"http://localhost:5173",*/"https://roomify-metau.onrender.com"],
-    origin: function(origin,callback){callback(null, true)},
+    origin: "https://roomify-metau.onrender.com",
     methods: ["GET","HEAD", "PUT", "PATCH", "POST", "DELETE"],
     credentials: true,
   });
