7dd67035fdd2a8a51b4c014db8cbb6b24d2111c7
Isabella Marin <marin.isabella.10@gmail.com>
2025-07-10 10:13:02 -0700

    fix: deploying again

--- server/server.js
+++ server/server.js
@@ -10,7 +10,7 @@ app.use(express.json());
 
 const corsConfig = cors({
     origin: ["http://localhost:5173", "https://roomify-metau.onrender.com/"],
-    methods: ["GET","HEAD", "PUT", "PATCH", "POST", "DELETE", "OPTIONS"],
+    methods: ["GET","HEAD", "PUT", "PATCH", "POST", "DELETE"],
     credentials: true,
   });
 
