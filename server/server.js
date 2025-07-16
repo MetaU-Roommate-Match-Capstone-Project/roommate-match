83832ea7e2ca6d029f885875aaaa719ad6a1b9ec
Isabella Marin <marin.isabella.10@gmail.com>
2025-07-16 11:37:16 -0700

    feat: manually creating cors config

--- server/server.js
+++ server/server.js
@@ -4,24 +4,35 @@ const app = express();
 const expressSession = require("express-session");
 const { PrismaSessionStore } = require("@quixo3/prisma-session-store");
 const { PrismaClient } = require("./generated/prisma");
-const cors = require("cors");
+// const cors = require("cors");
 
 app.use(express.json());
 
 app.use((req, res, next) => {
-  res.header("Access-Control-Allow-Origin", "https://roomify-metau.onrender.com");
+  const ALLOWED_ORIGINS = ['http://localhost:5173', 'https://roomify-metau.onrender.com'];
+
+  const origin = req.header('Origin');
+
+  if (ALLOWED_ORIGINS.includes(origin)) {
+    res.header("Access-Control-Allow-Origin", origin);
+    res.header("Access-Control-Allow-Credentials", true);
+    res.header("Access-Control-Allow-Headers", "content-type");
+    res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
+  }
+
+
   next();
 });
 
-const corsConfig = cors({
-    // origin: [/*"http://localhost:5173",*/"https://roomify-metau.onrender.com"],
-    origin: "https://roomify-metau.onrender.com",
-    methods: ["GET","HEAD", "PUT", "PATCH", "POST", "DELETE"],
-    credentials: true,
-  });
+// const corsConfig = cors({
+//     // origin: [/*"http://localhost:5173",*/"https://roomify-metau.onrender.com"],
+//     origin: "https://roomify-metau.onrender.com",
+//     methods: ["GET","HEAD", "PUT", "PATCH", "POST", "DELETE"],
+//     credentials: true,
+//   });
 
-// app.options('*', cors());
-app.use(corsConfig);
+// // app.options('*', cors());
+// app.use(corsConfig);
 
 app.use(
   expressSession({
