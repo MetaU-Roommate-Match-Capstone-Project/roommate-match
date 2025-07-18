fc9c6873473c0a85cd602bef9fb100df946b8c67
Isabella Marin <marin.isabella.10@gmail.com>
2025-07-18 12:08:18 -0700

    fix: added production url to PostPictureDisplay

--- client/src/components/PostPictureDisplay/PostPictureDisplay.jsx
+++ client/src/components/PostPictureDisplay/PostPictureDisplay.jsx
@@ -1,12 +1,13 @@
 import React from "react";
 import PictureSlideshow from "../PictureSlideshow/PictureSlideshow";
+import { getUrl } from "../../utils/url"
 
 const PostPictureDisplay = ({ pictures }) => {
   if (!pictures || pictures.length === 0) {
     return null;
   }
 
-  const imageUrls = pictures.map((pic) => `/api/post/picture/${pic.id}`);
+  const imageUrls = pictures.map((pic) => `${getUrl()}/api/post/picture/${pic.id}`);
 
   return <PictureSlideshow images={imageUrls} />;
 };
